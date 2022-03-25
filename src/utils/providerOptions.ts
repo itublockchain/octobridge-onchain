72803e992347712dd2ed6655b13e1b18a5c85a2e
asgarovf <farhad@getwhelp.com>
2022-03-25 14:40:39 +0100

    fix: rpc url

--- src/utils/providerOptions.ts
+++ src/utils/providerOptions.ts
@@ -4,8 +4,8 @@ export const providerOptions = {
   walletlink: {
     package: CoinbaseWalletSDK, // Required
     options: {
-      appName: "Web 3 Modal Demo", // Required
-      infuraId: process.env.INFURA_KEY, // Required unless you provide a JSON RPC url; see `rpc` below
+      appName: "Octobridge dApp", // Required Required unless you provide a JSON RPC url; see `rpc` below
+      rpc: "https://api.avax-test.network/ext/bc/C/rpc",
     },
   },
 };
