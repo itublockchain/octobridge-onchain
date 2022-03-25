4eee27872a3ed2d1f74bf299e1be2627ce4388d9
aalimsahin <alimsahin0007@gmail.com>
2022-03-26 02:13:45 +0300

    network

--- src/components/Bridge/Bridge.tsx
+++ src/components/Bridge/Bridge.tsx
@@ -1,15 +1,32 @@
 import { Typography } from "ui";
 import styles from "./Bridge.module.scss";
+import { Modal } from "ui";
+import {useModal} from 'hooks/useModal'
+import { Script } from "vm";
+import { IoMdMoon } from "react-icons/io";
+import { IoIosArrowDown } from 'react-icons/io'
 
 const Bridge = () => {
+  const fromModal = useModal();
   return (
+    <>
+    <Modal isOpen={fromModal.isOpen} close={fromModal.close}>
+          asdfasdfasddfs
+        </Modal>
     <div className={styles.wrapper}>
       <div className={styles.bridgeWrapper}>
         <Typography variant="title3" weight="semibold">
-          From:
+          From: 
         </Typography>
+        
+        <div onClick={fromModal.open} className={styles.networkWrapper}>
+        <IoMdMoon/>
+          AVALANCHE
+          <IoIosArrowDown/>
+        </div>
       </div>
     </div>
+    </>
   );
 };
 
