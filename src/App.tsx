35f8eb2b18399a902c35eeb861b6f885f8b6ab57
asgarovf <farhad@getwhelp.com>
2022-03-25 23:17:14 +0100

    feat: add router with dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
-import { Navbar } from "components";
-import { useAccounts } from "hooks/useAccounts";
-import { useModal } from "hooks/useModal";
-import { useRightNetwork } from "hooks/useRightNetwork";
+import { Bridge, Navbar } from "components";
 import { useTheme } from "hooks/useTheme";
-import { useWalletConnection } from "hooks/useWalletConnection";
 import { useWalletEvents } from "hooks/useWalletEvents";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Modal } from "ui";
 
 const MainApp = () => {
-  const testModal = useModal();
-  const { connectWallet, disconnect } = useWalletConnection({
-    autologin: true,
-  });
-  const { chainId, address } = useAccounts();
-  const { toggleTheme } = useTheme();
-  const { res } = useRightNetwork();
-
   return (
-    <>
-      <Modal isOpen={testModal.isOpen} close={testModal.close}>
-        selam
-      </Modal>
-      <div className="app">
-        <Navbar />
-      </div>
-    </>
+    <div className="app">
+      <Navbar />
+      <Bridge />
+    </div>
   );
 };
 
